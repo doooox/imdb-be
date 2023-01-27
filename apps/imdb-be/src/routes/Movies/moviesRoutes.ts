f87fc06c17042a159e13079ab1fe2581ee8fe1cc
doooox <dtopic12@gmail.com>
2023-01-27 11:45:48 +0100

    add movie routes

--- apps/imdb-be/src/routes/Movies/moviesRoutes.ts
+++ apps/imdb-be/src/routes/Movies/moviesRoutes.ts
@@ -1,12 +1,14 @@
 import * as express from 'express'
-import { createMovie, getMovies } from '../../controllers/Movies/moviesController'
-import isAuth from '../../middleware/Auth/authMiddleware'
+import { createMovie, getMovies, getSingleMovie } from '../../controllers/Movies/moviesController'
+import { isAdmin, isAuth } from '../../middleware/Auth/authMiddleware'
 import validateRequest from '../../middleware/validation/validationMiddleware'
 import moviesValidator from '../../validator/Movies/movieValidator'
 
 const moviesRouter = express.Router()
 
-moviesRouter.get("/", getMovies, isAuth)
-moviesRouter.post("/create", moviesValidator, validateRequest, createMovie)
+moviesRouter.get("/", isAuth, getMovies)
+moviesRouter.get("/:id", isAuth, getSingleMovie)
+moviesRouter.post("/create", isAuth, isAdmin, moviesValidator, validateRequest, createMovie)
+
 
 export default moviesRouter
