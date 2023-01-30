ca7214221ff93106a6543e40ed0c8d179d767e26
doooox <dtopic12@gmail.com>
2023-01-30 12:24:42 +0100

    implement search

--- apps/imdb-be/src/routes/Movies/moviesRoutes.ts
+++ apps/imdb-be/src/routes/Movies/moviesRoutes.ts
@@ -1,5 +1,5 @@
 import * as express from 'express'
-import { createMovie, getMovies, getSingleMovie } from '../../controllers/Movies/moviesController'
+import { createMovie, getMovies, getSearchedMovies, getSingleMovie } from '../../controllers/Movies/moviesController'
 import { isAdmin, isAuth } from '../../middleware/Auth/authMiddleware'
 import validateRequest from '../../middleware/validation/validationMiddleware'
 import moviesValidator from '../../validator/Movies/movieValidator'
@@ -7,6 +7,7 @@ import moviesValidator from '../../validator/Movies/movieValidator'
 const moviesRouter = express.Router()
 
 moviesRouter.get("/", isAuth, getMovies)
+moviesRouter.get("/find", isAuth, getSearchedMovies)
 moviesRouter.post("/create", isAuth, isAdmin, moviesValidator, validateRequest, createMovie)
 moviesRouter.get("/:id", isAuth, getSingleMovie)
 
